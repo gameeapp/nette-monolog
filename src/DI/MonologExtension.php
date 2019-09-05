606ccbf5069e609b8b855f5364e7636137c0a338
Pavel Janda <me@paveljanda.com>
2019-09-05 14:15:57 +0200

    Using nette 3

--- src/DI/MonologExtension.php
+++ src/DI/MonologExtension.php
@@ -5,17 +5,19 @@ declare(strict_types=1);
 namespace Gamee\Monolog\DI;
 
 use Monolog\Logger;
-use Nette\DI\Compiler;
 use Nette\DI\CompilerExtension;
+use Nette\DI\Definitions\FactoryDefinition;
+use Nette\DI\Definitions\ServiceDefinition;
 use Psr\Log\LoggerAwareInterface;
 
+/**
+ * @property-read array $config
+ */
 class MonologExtension extends CompilerExtension
 {
 
 	private const TAG_HANDLER = 'monolog.handler';
-
 	private const TAG_PROCESSOR = 'monolog.processor';
-
 	private const TAG_PRIORITY = 'monolog.priority';
 
 	/**
@@ -28,43 +30,41 @@ class MonologExtension extends CompilerExtension
 	];
 
 
-	public function loadConfiguration()
+	public function loadConfiguration(): void
 	{
 		parent::loadConfiguration();
 
-		$containerBuilder = $this->getContainerBuilder();
-		$config = $this->getConfig($this->defaults);
+		$builder = $this->getContainerBuilder();
+		$this->validateConfig($this->defaults);
 
-		foreach ($config['handlers'] as $handlerName => $implementation) {
-			Compiler::loadDefinitions($containerBuilder, [
+		foreach ($this->config['handlers'] as $handlerName => $implementation) {
+			$this->compiler->loadDefinitionsFromConfig([
 				$serviceName = $this->prefix('handler.' . $handlerName) => $implementation,
 			]);
-			$containerBuilder->getDefinition($serviceName)
+
+			$builder->getDefinition($serviceName)
 				->addTag(self::TAG_HANDLER)
-				->addTag(self::TAG_PRIORITY, ctype_digit($handlerName) ? $handlerName : 0)
-			;
+				->addTag(self::TAG_PRIORITY, ctype_digit($handlerName) ? $handlerName : 0);
 		}
 
-		$containerBuilder->addDefinition($this->prefix('logger'))
-			->setClass(Logger::class, [$config['name']])
-		;
-
-		foreach ($config['processors'] as $processorName => $implementation) {
-			Compiler::loadDefinitions($containerBuilder, [
+		foreach ($this->config['processors'] as $processorName => $implementation) {
+			$this->compiler->loadDefinitionsFromConfig([
 				$serviceName = $this->prefix('processor.' . $processorName) => $implementation,
 			]);
-			$containerBuilder->getDefinition($serviceName)
+
+			$builder->getDefinition($serviceName)
 				->addTag(self::TAG_PROCESSOR)
-				->addTag(self::TAG_PRIORITY, ctype_digit($processorName) ? $processorName : 0)
-			;
+				->addTag(self::TAG_PRIORITY, ctype_digit($processorName) ? $processorName : 0);
 		}
 	}
 
 
-	public function beforeCompile()
+	public function beforeCompile(): void
 	{
 		$builder = $this->getContainerBuilder();
-		$logger = $builder->getDefinition($this->prefix('logger'));
+
+		$logger = $builder->addDefinition($this->prefix('logger'))
+			->setFactory(Logger::class, [$this->config['name']]);
 
 		foreach ($handlers = $this->findByTagSorted(self::TAG_HANDLER) as $serviceName => $meta) {
 			$logger->addSetup('pushHandler', ['@' . $serviceName]);
@@ -75,15 +75,22 @@ class MonologExtension extends CompilerExtension
 		}
 
 		foreach ($builder->findByType(LoggerAwareInterface::class) as $service) {
-			$service->addSetup('setLogger', ['@' . $this->prefix('logger')]);
+			if ($service instanceof ServiceDefinition) {
+				$service->addSetup('setLogger', ['@' . $this->prefix('logger')]);
+			} elseif ($service instanceof FactoryDefinition) {
+				$service->getResultDefinition()->addSetup('setLogger', ['@' . $this->prefix('logger')]);
+			} else {
+				throw new \UnexpectedValueException;
+			}
 		}
 	}
 
 
-	protected function findByTagSorted($tag)
+	protected function findByTagSorted(string $tag): array
 	{
 		$builder = $this->getContainerBuilder();
 		$services = $builder->findByTag($tag);
+
 		uksort($services, function ($nameA, $nameB) use ($builder) {
 			$priorityA = $builder->getDefinition($nameA)->getTag(self::TAG_PRIORITY) ?: 0;
 			$priorityB = $builder->getDefinition($nameB)->getTag(self::TAG_PRIORITY) ?: 0;
@@ -93,5 +100,4 @@ class MonologExtension extends CompilerExtension
 
 		return $services;
 	}
-
 }
